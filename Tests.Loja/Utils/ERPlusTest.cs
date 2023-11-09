63bc1f0d0487d33b72b3d1e6cd56749b997df2a3
waltereidi <walter-eidi@hotmail.com>
2023-11-08 21:15:38 -0300

    Adicionado Planilha excel colorida nos testes de unidade.

--- Tests.Loja/Utils/ERPlusTest.cs
+++ Tests.Loja/Utils/ERPlusTest.cs
@@ -1,8 +1,10 @@
 ﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,23 +35,47 @@ namespace Tests.Loja.Utils
            
         }
         [TestMethod]
-        public void WriteExcelFileCanReadFile()
+        public void WriteColoredExcelFileCanReadFile()
         {
+            //setup 
+            string filepath = "C:\\Users\\walte\\Desktop\\Pingendo\\arq3.xlsx";
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            
             DataTable dt = new DataTable();
-            dt.Columns.Add("Nome");
-            dt.Columns.Add("Telefone");
-            dt.Rows.Add("1", "11111");
-            dt.Rows.Add("2", "22222");
+            
+            
+            for(int i = 0; i< 100; i++ )
+            {
+                dt.Columns.Add(i.ToString());
+               
+            }
+            for (int k = 0; k < 100; k++)
+            {
+                dt.Rows.Add(k, k.ToString() + "SDSD");
+            }
             FileInfo fileInfoTemplate = new FileInfo("C:/arq2.xlsx");
 
-            OfficeOpenXml.ExcelPackage excel = new ExcelPackage(fileInfoTemplate);
-
+            //Action
+            OfficeOpenXml.ExcelPackage excel = new ExcelPackage();
             ExcelWorksheet worksheet = excel.Workbook.Worksheets.Add("teste");
+
             worksheet.Cells["A1"].LoadFromDataTable(dt, true);
 
-            FileInfo fileInfo = new FileInfo("C:\\Users\\walte\\Desktop\\Pingendo\\arq3.xlsx");
+            
+            worksheet.Cells["A1:Z100"].Style.Fill.PatternType = ExcelFillStyle.Solid;
+
+            worksheet.Cells["A1:Z100"].Style.Border.Top.Style=ExcelBorderStyle.Thin;
+            worksheet.Cells["A1:Z100"].Style.Font.Bold=true;
+            worksheet.Cells["A1:Z100"].Style.Font.Italic=true;
+            worksheet.Cells["A1:Z100"].Style.Fill.BackgroundColor.SetColor(Color.Red);
+            FileInfo fileInfo = new FileInfo(filepath);
             excel.SaveAs(fileInfo);
 
+            //Assert
+            Assert.IsNotNull(File.Exists(filepath));
+
+
         }
+        
     }
 }
