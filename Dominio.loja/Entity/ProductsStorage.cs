a91eb66018c99149a4127a571d7a884bb2e23668
waltereidi <walter-eidi@hotmail.com>
2023-11-11 17:55:57 -0300

    Adicionado entidade

--- Dominio.loja/Entity/ProductsStorage.cs
+++ Dominio.loja/Entity/ProductsStorage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace Dominio.loja.Entity
     public class ProductsStorage
     {
         public int Quantity { get; set; }
-        
+        [StringLength(255)]
         public string Description { get; set; }
 
         public Products Product { get; set; } 
