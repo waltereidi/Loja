e3f41108171dccc374ef4281699d25032d2ddfd6
waltereidi <walter-eidi@hotmail.com>
2024-02-16 21:08:13 -0300

    Adicionado projeto RabbitMQ .loja para gerenciar filas

---
+++ RabbitMQ.loja/FileUploadQueue.cs
@@ -0,0 +1,67 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Channels;
+
+namespace RabbitMQ.loja
+{
+    public class FileUploadQueue
+    {
+        private readonly string _queueName = "fileUploadQueue";
+        private readonly ConnectionFactory _factory;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private EventingBasicConsumer _consumer { get; set; }
+        public FileUploadQueue(string hostname)
+        {
+            _factory = new ConnectionFactory { HostName = hostname };
+            _connection = _factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare(queue: _queueName,
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+            _channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            _consumer = new EventingBasicConsumer(_channel);
+            _channel.BasicConsume(queue: _queueName,
+                                 autoAck: false,
+                                 consumer: _consumer);
+
+
+            _consumer.Received += (model, ea) =>
+            {
+                string response = string.Empty;
+
+                var body = ea.Body.ToArray();
+                var props = ea.BasicProperties;
+                var replyProps = _channel.CreateBasicProperties();
+                replyProps.CorrelationId = props.CorrelationId;
+
+                try
+                {
+                    response = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [.] {response}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($" [.] {e.Message}");
+                    response = string.Empty;
+                }
+                finally
+                {
+                    //Send back the response to the caller.
+                    var responseBytes = Encoding.UTF8.GetBytes(response);
+                    _channel.BasicPublish(exchange: string.Empty,
+                                         routingKey: props.ReplyTo,
+                                         basicProperties: replyProps,
+                                         body: responseBytes);
+                    _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                }
+            };
+        }
+
+
+    }
+}
