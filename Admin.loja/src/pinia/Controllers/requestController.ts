d95f10590b57301bacf78ffddd670b5e51ccaf5e
walter-eidi <walter-eidi@hotmail.com>
2024-11-13 21:03:20 -0300

    retirado o dia para aprender curry em funções de aridade maior que 3

--- Admin.loja/src/pinia/Controllers/requestController.ts
+++ Admin.loja/src/pinia/Controllers/requestController.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { IDependencyInjection } from '@/pinia/Interfaces/IDependencyInjection';
 import { LogController } from '@/pinia/Controllers/LogController';
 import { LogSeverity } from '../Dto/Log';
@@ -8,39 +7,53 @@ export class RequestController implements  IDependencyInjection{
     constructor()
     {
         this.log.addLog( `request constructor` , LogSeverity.Initialization );
-        this.setDefaultHeaders();
     }
-    public getConfigurations(){
-        return axios.defaults.headers;
-    }
-    private setDefaultHeaders() {
-        this.log.addLog( `started configuration` , LogSeverity.Event );
+    
+    private getDefaultHeader() : Headers
+    {
+        this.log.addLog("set headers" , LogSeverity.Event)
 
-        //axios.defaults.baseURL = appSettings.ApiUrl;
-        axios.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
-        axios.defaults.headers.post['Accept'] = '*/*';
-        axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-        axios.defaults.headers.post['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,PATCH,OPTIONS';
-        axios.defaults.headers.post['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With';
-        axios.defaults.headers.put = axios.defaults.headers.post;
-        axios.defaults.headers.options = axios.defaults.headers.post;
+        const headers = new Headers();
         
-        axios.defaults.transformRequest =(data, headers)=>{
-            // Do whatever you want to transform the data
-            return data;
-          };
+        headers.append('Content-Type' , 'application/json; charset=utf-8')
+        headers.append('Accept' , '*/*')
+        headers.append('Access-Control-Allow-Origin' , '*')
+        headers.append('Access-Control-Allow-Methods' , 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+        headers.append('Access-Control-Allow-Headers' , 'Origin, X-Requested-With')
 
-        axios.defaults.transformResponse = (data) =>{
-            // Do whatever you want to transform the data
-        
-            return data;
-        };
-
-
-        this.log.addLog( `configured axios ${axios.defaults}` , LogSeverity.Parameter );
+        return headers;
     }
-    public post =(url:string , data:any ) => axios.post( url ,data );
-    public get =(url:string ) => axios.post( url );
+    /**
+     * Curry
+     */
+    post = (url:string , data:any , headers:any, request:Request ) =>
+    {
+        // const request = new Request(url ,{
+        //     method: 'POST', 
+        //     body: data, 
+        //     headers: headers??this.getDefaultHeader()
+        // })
+        return fetch(request);
+    }
+    
+
+    private curry(fn:any){
+        return fn.length === 0 
+        ? fn()
+        : (x:any) => (fn.bind(null , x ));
+    }
+    f1 = this.curry(post)
+    type Curry<P, R> = P extends [infer H]
+    ? (arg: H) => R // only 1 arg
+    : P extends [infer H, ...infer T] // 2 or more args
+    ? (arg: H) => Curry<[...T], R>
+    : never;
+    
+    public get(url:string )
+    {
+
+
+    }
     public put =(url:string , data:any ) => axios.put( url ,data );
     public delete =(url:string ) => axios.delete( url);
 
--- Admin.loja/src/tests/unit/pinia/controllers/requestController.spec.ts
+++ Admin.loja/src/tests/unit/pinia/controllers/requestController.spec.ts
@@ -2,12 +2,14 @@ import { expect, test } from 'vitest'
 import { RequestController } from '@/pinia/Controllers/requestController'
 import { LogSeverity } from '@/pinia/Dto/Log';
 
+const request = new RequestController();
 test('add log returns has error when add success false log', () => {
-    const request = new RequestController();
+    
     const result = request.get('http://testCase.com')
-        .then((result) =>expect( result.status).toBe(200))
     
-    
+})
+test('curry' ,() =>{
 
-    
+    expect(request.postCurry('')('')('')).toBe('')
+
 })
